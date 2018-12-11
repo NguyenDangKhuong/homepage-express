f2c3d188d7eba29a7c6bd07d54cd3b76af5f1eef
nguyendangkhuong96 <nguyendang.khuong@asoview.co.jp>
2018-12-11 11:07:11 +0700

    add uglifyjs-webpack-plugin for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 const glob = require('glob')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
 const jsEntries = {}

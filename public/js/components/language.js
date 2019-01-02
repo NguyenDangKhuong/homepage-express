44f182ef1f04f7bf3b6ad7ca167a3687f82e9f12
nguyendangkhuong96 <nguyendang.khuong@asoview.co.jp>
2019-01-02 13:03:30 +0700

    fix footer and i18n

--- public/js/components/language.js
+++ public/js/components/language.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export default class Language extends React.Component{
-  constructor(props){
+let lang = localStorage.getItem('lang')
+
+export default class Language extends React.Component {
+  constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       languageTitle: <span className="language-title-icons">EN</span>
+    }
+  }
+
+  componentDidMount() {
+    if (lang === 'en') {
+      this.setState({
+        languageTitle: <span className="language-title-icons">EN</span>
+      })
+    } else if (lang === 'vi') {
+      this.setState({
+        languageTitle: <span className="language-title-icons">VN</span>
+      })
+    } else if (lang === 'jp') {
+      this.setState({
+        languageTitle: <span className="language-title-icons">JP</span>
+      })
     }
   }
 
@@ -13,26 +31,27 @@ export default class Language extends React.Component{
     this.setState({
       languageTitle: <span className="language-title-icons">EN</span>
     })
+    localStorage.setItem('lang', 'en')
   }
 
   changeVietnamese = () => {
     this.setState({
       languageTitle: <span className="language-title-icons">VN</span>
     })
-
+    localStorage.setItem('lang', 'vi')
   }
 
   changeJapanese = () => {
-
     this.setState({
       languageTitle: <span className="language-title-icons">JP</span>
     })
+    localStorage.setItem('lang', 'jp')
   }
 
-  render(){
+  render() {
     let {languageTitle} = this.state
 
-    return(
+    return (
       <div className="language-dropdown">
         <div className="dropdown">
           <button className="btn dropdown-toggle " type="button" id="dropdownMenuButton"
@@ -40,17 +59,21 @@ export default class Language extends React.Component{
             {languageTitle}
           </button>
           <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="/change-lang/en" onClick={this.changeEnglish}><div className="language-title">English</div></a>
-            <a className="dropdown-item" href="/change-lang/vi" onClick={this.changeVietnamese}><div className="language-title">Tiếng
-              Việt</div></a>
-            <a className="dropdown-item" href="#" onClick={this.changeJapanese}><div className="language-title">日本語</div></a>
+            <a className="dropdown-item" href="/change-lang/en" onClick={this.changeEnglish}>
+              <div className="language-title">English</div>
+            </a>
+            <a className="dropdown-item" href="/change-lang/vi" onClick={this.changeVietnamese}>
+              <div className="language-title">Tiếng Việt</div>
+            </a>
+            <a className="dropdown-item" href="#" onClick={this.changeJapanese}>
+              <div className="language-title">日本語</div>
+            </a>
           </div>
         </div>
       </div>
     )
   }
 }
-
 
 
 ReactDOM.render(<Language/>, document.getElementById('language-dropdown'))
